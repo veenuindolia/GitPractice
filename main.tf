f8a08f01a2896b639b8518b0a941522ad42eb6aa
veenuindolia <veenuindolia@gmail.com>
2026-05-27 01:16:35 +0530

    Add new Azure resource group 'veenu6'

--- main.tf
+++ main.tf
@@ -21,3 +21,10 @@ resource "azurerm_resource_group" "veenu5" {
     location = "westus" 
   
 }
+
+resource "azurerm_resource_group" "veenu6" {
+    name = "veenu6"
+    location = "westus" 
+  
+}
+
